ddbed23ca52abec0d5783d214dd8fd6777f311bf
barshaparajuli <40861964+barshaparajuli@users.noreply.github.com>
2021-07-21 10:23:05 -0400

    Iteration 2 - Profile Button Completed?

--- KWKFinalProject/KWKFinalProject/ViewController.swift
+++ KWKFinalProject/KWKFinalProject/ViewController.swift
@@ -8,13 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var enterName: UITextField!
+    @IBOutlet weak var name: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func nameButton(_ sender: UIButton) {
+        if let newName = enterName.text {
+                name.text = newName
+        }
+    }
 
 }
 
